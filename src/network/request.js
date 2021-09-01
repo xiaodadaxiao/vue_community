a1a8e45b944cbd20b89d722ac01d80151ec3a69d
cheng <2492126439@qq.com>
2021-09-01 17:46:42 +0800

    优化NProgress

--- src/network/request.js
+++ src/network/request.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import NProgress from "plugins/nprogress";
+import { Message } from "element-ui";
 export default function request(config) {
   //创建axios的实例一
   //( 目前项目只有一个实例 ,不同参数可以创建不同实例,对应不同网络请求)
@@ -23,7 +24,9 @@ export default function request(config) {
       return config;
     },
     (err) => {
+      NProgress.done();
       console.log(err);
+      Message.error('网络请求失败:' + err)
     }
   );
   //响应拦截器
@@ -34,7 +37,9 @@ export default function request(config) {
       return config.data;
     },
     (err) => {
+      NProgress.done();
       console.log(err);
+      Message.error('网络请求失败:' + err)
     }
   );
 
@@ -42,8 +47,7 @@ export default function request(config) {
   return instance(config);
 }
 export function request2() {
-  //创建axios的实例一
-  //( 目前项目只有一个实例 ,不同参数可以创建不同实例,对应不同网络请求)
+  //创建axios的实例二
   const instance2 = axios.create({
     //默认请求url
 
@@ -54,7 +58,7 @@ export function request2() {
   //请求拦截器
   instance2.interceptors.request.use(
     (config) => {
-      NProgress.start();
+      //NProgress.start();
       config.headers = {
         //请求头携带token
         token: localStorage.getItem("token"),
@@ -63,18 +67,22 @@ export function request2() {
       return config;
     },
     (err) => {
+      NProgress.done();
       console.log(err);
+      Message.error('网络请求失败:' + err)
     }
   );
   //响应拦截器
   instance2.interceptors.response.use(
     (config) => {
-      NProgress.done();
+      //NProgress.done();
       //最后传递处理后的数据
       return config.data;
     },
     (err) => {
+      NProgress.done();
       console.log(err);
+      Message.error('网络请求失败:' + err)
     }
   );
 
