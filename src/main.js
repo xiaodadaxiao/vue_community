69eafa3c7596f4617c814e77d6c3349a7157c0a9
dadada <2492126439@qq.com>
2024-12-11 09:58:46 +0800

    test:fix mainjs

--- src/main.js
+++ src/main.js
@@ -1,29 +1,28 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
 /* 基础样式 */
-import "assets/css/base.css";
+import 'assets/css/base.css'
 
 /* 插件 */
 //挂载element-ui
-import "plugins/elementUI";
-import "plugins/vueXss"
-
+import 'plugins/elementUI'
+import 'plugins/vueXss'
 
 /* 事件总线 */
-Vue.prototype.$bus = new Vue(); //vue实例可以作为事件总线
+Vue.prototype.$bus = new Vue() //vue实例可以作为事件总线
 
 /* 过滤器 */
-import { dateFormat } from "utils/filter";
-Vue.filter("dateFormat", dateFormat);
+import { dateFormat } from 'utils/filter'
+Vue.filter('dateFormat', dateFormat)
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+console.log('main')
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
